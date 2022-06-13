ca03223f920963f8d8d589e49c4344830afe67d3
MiladSadeghi <miladsadeghi2323@gmail.com>
2022-06-13 23:19:59 +0430

    CRM - menu content chooser

--- crm/src/JS/main.js
+++ crm/src/JS/main.js
@@ -1,10 +1,34 @@
 const dropDownMenu = document.querySelector('.dropdown');
 const dropDownMenuOption = document.querySelector('.option');
-const profileBtnArrow = document.querySelector(".bi-arrow-down")
+const menuBtnArrow = document.querySelectorAll(".show-list");
+const menuBtnDrowDown = document.querySelectorAll(".dropdown1");
+const profileArrow = document.querySelectorAll(".profile-arrow");
+const profileBtn = document.querySelector("#profile-name");
+const showMenu = document.querySelector("#showMegaMenu");
+const category = document.querySelector(".category");
+const mainBody = document.querySelector(".main-body");
 
-document.addEventListener("DOMContentLoaded", ()=> {
-  dropDownMenu.addEventListener('click', ()=> {
-    profileBtnArrow.classList.toggle('rotateArrow');
+document.addEventListener("DOMContentLoaded", () => {
+  dropDownMenu.addEventListener('click', (element) => {
+    profileBtn.children[0].classList.toggle('rotateArrow');
     dropDownMenuOption.classList.toggle('active');
+  })
+
+  menuBtnArrow.forEach((element, index) => {
+    element.addEventListener('click', (e) => {
+      profileArrow[index].classList.toggle('rotateArrow');
+      menuBtnDrowDown[index].classList.toggle("show");
+
+    });
+  })
+
+  showMenu.addEventListener("change", (e) => {
+    if (showMenu.checked) {
+      category.style.left = "-100%";
+      mainBody.classList.toggle("col-md-9");
+    } else {
+      category.style.left = "0";
+      mainBody.classList.toggle("col-md-9");
+    }
   })
 })
