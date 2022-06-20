765d99e49ceb705decbda7eb77736a1b557d79ae
Milad Sadeghi <miladsadeghi2323@gmail.com>
2022-06-21 01:37:43 +0430

    CRM - refactor from a function to a component-base program

--- crm/src/JS/main.js
+++ crm/src/JS/main.js
@@ -1,3 +1,5 @@
+import { addProduct } from "/crm/src/components/Product/Add/addProduct.js";
+
 const dropDownMenu = document.querySelector('.dropdown');
 const dropDownMenuOption = document.querySelector('.option');
 const menuBtnArrow = document.querySelectorAll(".show-list");
@@ -11,6 +13,7 @@ const mainBody = document.querySelector(".main-body");
 let crmUser;
 document.addEventListener("DOMContentLoaded", (e) => {
   loadUser();
+  window.customElements.define('add-product', addProduct);
   dropDownMenu.addEventListener('click', (element) => {
     profileBtn.children[0].classList.toggle('rotateArrow');
     dropDownMenuOption.classList.toggle('active');
@@ -38,13 +41,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
         userAdd();
         break;
       case "product":
-        productAdd();
+        mainBody.innerHTML = `<add-product></add-product>`;
         break;
     }
-  })
-
-  mainBody.addEventListener("click", (e) => {
-    product(e);
   })
 })
 
@@ -59,229 +58,4 @@ function loadUser() {
   })();
 }
 
-function product(e) {
-  if (e.target.classList.contains("product-add-specification")) {
-    e.stopPropagation();
-    document.querySelector(".product-specification").parentElement.innerHTML += `<div class="input-group mt-2 product-specification">
-    <input type="text" placeholder="title" class="form-control shadow-lg">
-    <input type="text" placeholder="discription" class="form-control shadow-lg">
-    <button class="btn btn-danger product-remove-specification" type="button"></button>
-    </div>`
-  }
-  if (e.target.classList.contains("product-remove-specification")) {
-    e.target.parentElement.remove();
-  }
-  if (e.target.classList.contains("bi-wrench-adjustable-circle")) {
-    e.target.addEventListener("shown.bs.popover", (el) => {
-      let button = document.querySelector(`#${el.target.getAttribute("aria-describedby")} button`);
-      button.addEventListener("click", (ele) => {
-        productPhotoContentType(ele.target.previousElementSibling.value).then(res => {
-          if (res) {
-            e.target.parentElement.style.background = `url(${ele.target.previousElementSibling.value})`;
-            e.target.parentElement.style.backgroundColor = `#fff`;
-          }
-        })
-      })
-    })
-  }
-  if (e.target.classList.contains("add-photo-div")) {
-    let photosList = document.querySelector(".pr-ph-ph");
-    loadPopOver();
-    if (photosList.childElementCount < 6) {
-      photosList.innerHTML += `<div class="glass second-glass w-100 fs-4 rounded shadow-lg align-items-center justify-content-center"><i class="bi bi-dash-circle remove-photo-div"></i><a href="#" class="bi bi-wrench-adjustable-circle" data-bs-toggle="popover"></a></div>`
-      loadPopOver();
-    } else {
-      loadPopOver();
-    }
-  }
-  if (e.target.classList.contains("remove-photo-div")) {
-    e.target.parentElement.remove();
-    loadPopOver();
-  }
-}
-
-function loadPopOver() {
-  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, {
-    container: 'body',
-    title: 'Add Photo Link',
-    html: true,
-    placement: 'bottom',
-    sanitize: false,
-    content() {
-      return document.querySelector('#PopoverContent').innerHTML;
-    }
-  }));
-}
-
-async function productPhotoContentType(get_url) {
-  const response = await fetch(get_url);
-  return response.headers.get('content-type').includes("image") ? true : false;
-}
-
-const uniqeID = () => {
-  let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
-  for (let i = 0; i < 16; i++) {
-    result += characters.charAt(Math.floor(Math.random() *
-      charactersLength));
-  }
-  return result;
-};
-
-function detectURLs(message) {
-  let urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
-  return String(message.match(urlRegex));
-}
-
-async function postProduct(apiLink, body) {
-  let response = await fetch(apiLink, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(body)
-  })
-  let result = await response.json();
-  return await result;
-}
-
-function addProduct(e) {
-  e.preventDefault();
-  let productCategory = document.querySelector("#product-category-add");
-  let product = {
-    timeAddProduct: new Date(),
-    userAddProduct: [crmUser.userName, crmUser.profileID],
-    productID: uniqeID(),
-    comments: "",
-    productName: document.querySelector("#product-name-add").value,
-    productModel: document.querySelector("#product-model-add").value,
-    productBrand: document.querySelector("#product-brand-add").value,
-    productPrice: document.querySelector("#product-price-add").value,
-    productDescription: document.querySelector("#product-description-add").value,
-    productTags: (document.querySelector("#product-add-tag").value).split(",").map(el => el.trim()).filter((a) => a),
-    productFeature: [],
-    productSpecification: [],
-    productPhoto: [],
-  }
-  let productAddLog = {
-    time: new Date(),
-    user: [crmUser.userName, crmUser.profileID],
-    productID: product.productID,
-    logID: uniqeID(),
-    logType: "add product",
-    category: productCategory.value,
-    subCategory: productCategory.options[productCategory.selectedIndex].text,
-  }
-  product["productCategory"] = productCategory.value;
-  product["productSubCategory"] = productCategory.options[productCategory.selectedIndex].text;
-  document.querySelectorAll("#product-feature li").forEach(element => {
-    if (element.children[0].value !== "" && element.children[1].value !== "") {
-      product["productFeature"].push({
-        title: element.children[0].value,
-        discription: element.children[1].value
-      })
-    }
-  });
-  document.querySelectorAll(".product-specification").forEach(element => {
-    if (element.children[0].value !== "" && element.children[1].value !== "") {
-      product["productSpecification"].push({
-        title: element.children[0].value,
-        discription: element.children[1].value
-      })
-    }
-  });
-  document.querySelectorAll(".glass").forEach(element => {
-    if (detectURLs(element.style.background)) {
-      product["productPhoto"].push(detectURLs(element.style.background))
-    }
-  });
-  let apiProduct = `https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/product/${product.productID}.json`
-  let apiLog = `https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/log/${productAddLog.logID}.json`
-  postProduct(apiProduct, product);
-  postProduct(apiLog, productAddLog);
-}
-
-function productAdd(event) {
-  mainBody.innerHTML = `<form class="product" onsubmit="addProduct(event)">
-  <div class="pr-na"><input placeholder="Product Name" type="text" class="form-control shadow-lg"
-      id="product-name-add"></div>
-  <div class="pr-mo"><input placeholder="Product Model" type="text" class="form-control shadow-lg"
-      id="product-model-add"></div>
-  <div class=" pr-fe">
-    <ul class="mb-0 list-unstyled" id="product-feature">
-      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
-      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
-      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
-      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
-      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
-      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
-    </ul>
-  </div>
-  <div class="d-flex pr-ph">
-    <div class="glass pr-ph-main first-glass d-flex fs-2 rounded shadow-lg align-items-center justify-content-evenly">
-    <i class="bi bi-plus-circle add-photo-div"></i>
-      <a href="#" class="bi bi-wrench-adjustable-circle" data-bs-toggle="popover"></a>
-    </div>
-  </div>
-  <div class="pr-ph-ph w-100">
-  </div>
-  <div class="d-flex pr-di"><textarea class="form-control shadow-lg" placeholder="Discription"
-      style="resize: none;" id="product-description-add" cols="20" rows="5"></textarea></div>
-  <div class="d-flex pr-br"><input placeholder="Brand" type="text" class="form-control w-100 shadow-lg"
-      id="product-brand-add"></div>
-  <div class="d-flex pr-pr"><input placeholder="Price" type="text" class="form-control w-100 shadow-lg"
-      id="product-price-add"></div>
-  <div class="pr-sp" >
-    <div class="input-group product-specification">
-      <input type="text" placeholder="title" class="form-control shadow-lg">
-      <input type="text" placeholder="discription" class="form-control shadow-lg">
-      <button class="btn btn-success product-add-specification" type="button"></button>
-      <button class="btn btn-danger product-remove-specification" disabled type="button"></button>
-    </div>
-  </div>
-  <div class="pr-ca">
-    <select class="form-select" aria-label="Default select example" id="product-category-add">
-      <option value="" disabled selected>Open this select menu</option>
-      <option value="digital">Accessories</option>
-      <option value="digital">Mobile phone</option>
-      <option value="digital">Smart watch</option>
-      <option value="digital">Computer accessories</option>
-      <option value="digital">Laptop</option>
-      <option value="clothing">Men clothing</option>
-      <option value="clothing">Ladies clothing</option>
-      <option value="clothing">Childish clothing</option>
-      <option value="health">Shampoo</option>
-      <option value="health">Skin cream</option>
-      <option value="health">Oral health</option>
-      <option value="health">Skin care</option>
-      <option value="health">Skin care</option>
-      <option value="health">Face makeup</option>
-      <option value="health">Hairdressing</option>
-      <option value="sport">Men's sportswear</option>
-      <option value="sport">Women's sportswear</option>
-      <option value="sport">Sports watch</option>
-      <option value="sport">Bodybuilding</option>
-      <option value="sport">Mountaineering</option>
-      <option value="sport">Sporting goods</option>
-      <option value="travel">Suitcases and bags</option>
-      <option value="travel">Tent</option>
-      <option value="travel">Underlayment</option>
-      <option value="home">Video and Audio</option>
-      <option value="home">Decorative</option>
-      <option value="home">Carpet</option>
-      <option value="home">Electrical appliances</option>
-      <option value="home">Kitchen</option>
-      <option value="home">Detergents</option>
-      <option value="book">Book and magazine</option>
-      <option value="book">Audio book</option>
-      <option value="book">Paper</option>
-      <option value="stationery">Stationery</option>
-      <option value="stationery">Painting tools</option>
-    </select>
-    </div>
-    <input type="text" id="product-add-tag" placeholder="Tag, Split tag with comma" class="form-control pr-ta">
-  <input id="submit-product" value="Add Product" type="submit" class="btn btn-success pr-su"></form>`;
-  loadPopOver();
-}
+export { crmUser };
---
+++ crm/src/components/Product/Add/addProduct.js
@@ -0,0 +1,271 @@
+import { crmUser } from "../../../JS/main.js";
+
+const template = document.createElement("template");
+template.innerHTML = `
+<link rel="stylesheet" href="/crm/src/components/Product/Add/style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+<form class="product">
+  <div class="pr-na">
+    <input placeholder="Product Name" type="text" class="form-control shadow-lg"id="product-name-add">
+  </div>
+  <div class="pr-mo"><input placeholder="Product Model" type="text" class="form-control shadow-lg"id="product-model-add"></div>
+  <div class=" pr-fe">
+    <ul class="mb-0 list-unstyled" id="product-feature">
+      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
+      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
+      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
+      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
+      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
+      <li class="input-group"><input placeholder="feature" type="text" class="form-control"><input placeholder="discription" type="text" class="form-control"></li>
+    </ul>
+  </div>
+  <div class="d-flex pr-ph">
+    <div class="glass pr-ph-main first-glass d-flex fs-2 rounded shadow-lg align-items-center justify-content-evenly"><i class="bi bi-plus-circle add-photo-div"></i><a href="#" class="bi bi-wrench-adjustable-circle" data-bs-toggle="popover"></a></div>
+  </div>
+  <div class="pr-ph-ph w-100"></div>
+  <div class="d-flex pr-di"><textarea class="form-control shadow-lg" placeholder="Discription"style="resize: none;" id="product-description-add" cols="20" rows="5"></textarea></div>
+  <div class="d-flex pr-br"><input placeholder="Brand" type="text" class="form-control w-100 shadow-lg"id="product-brand-add"></div>
+  <div class="d-flex pr-pr"><input placeholder="Price" type="text" class="form-control w-100 shadow-lg"id="product-price-add"></div>
+  <div class="pr-sp">
+    <div class="input-group product-specification"><input type="text" placeholder="title" class="form-control shadow-lg"><input type="text" placeholder="discription" class="form-control shadow-lg"><button class="btn btn-success product-add-specification" type="button"></button><button class="btn btn-danger product-remove-specification" disabled type="button"></button></div>
+  </div>
+  <div class="pr-ca">
+    <select class="form-select" aria-label="Default select example" id="product-category-add">
+      <option value="" disabled selected>Open this select menu</option>
+      <option value="digital">Accessories</option>
+      <option value="digital">Mobile phone</option>
+      <option value="digital">Smart watch</option>
+      <option value="digital">Computer accessories</option>
+      <option value="digital">Laptop</option>
+      <option value="clothing">Men clothing</option>
+      <option value="clothing">Ladies clothing</option>
+      <option value="clothing">Childish clothing</option>
+      <option value="health">Shampoo</option>
+      <option value="health">Skin cream</option>
+      <option value="health">Oral health</option>
+      <option value="health">Skin care</option>
+      <option value="health">Skin care</option>
+      <option value="health">Face makeup</option>
+      <option value="health">Hairdressing</option>
+      <option value="sport">Men's sportswear</option>
+      <option value="sport">Women's sportswear</option>
+      <option value="sport">Sports watch</option>
+      <option value="sport">Bodybuilding</option>
+      <option value="sport">Mountaineering</option>
+      <option value="sport">Sporting goods</option>
+      <option value="travel">Suitcases and bags</option>
+      <option value="travel">Tent</option>
+      <option value="travel">Underlayment</option>
+      <option value="home">Video and Audio</option>
+      <option value="home">Decorative</option>
+      <option value="home">Carpet</option>
+      <option value="home">Electrical appliances</option>
+      <option value="home">Kitchen</option>
+      <option value="home">Detergents</option>
+      <option value="book">Book and magazine</option>
+      <option value="book">Audio book</option>
+      <option value="book">Paper</option>
+      <option value="stationery">Stationery</option>
+      <option value="stationery">Painting tools</option>
+    </select>
+  </div>
+  <input type="text" id="product-add-tag" placeholder="Tag, Split tag with comma" class="form-control pr-ta"><input id="submit-product" value="Add Product" type="submit" class="btn btn-success pr-su">
+</form>
+<div id="PopoverContent" class="d-none">
+<div class="input-group">
+  <input type="text" class="form-control" placeholder="URL">
+  <button id="submit-photo-link" class="btn btn-outline-primary bi bi-arrow-right fs-5" submit-photo-url type="button" data-toggle="popover" data-placement="bottom"
+    data-html="true">
+  </button>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+crossorigin="anonymous"></script>`;
+
+class addProduct extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.productAddSpecification = this.shadowRoot.querySelector(".product-add-specification");
+    this.productRemoveSpecification = this.shadowRoot.querySelectorAll(".product-remove-specification");
+    this.productSpecification = this.shadowRoot.querySelector(".pr-sp");
+    this.mainPhoto = this.shadowRoot.querySelector(".pr-ph");
+    this.subPhotos = this.shadowRoot.querySelector(".pr-ph-ph");
+    this.addPhotoDivBtn = this.shadowRoot.querySelector(".add-photo-div");
+    this.productAddForm = this.shadowRoot.querySelector(".product");
+  }
+
+  addSpecification = () => {
+    this.productSpecification.insertAdjacentHTML("beforeend", `<div class="input-group mt-2 product-specification">
+    <input type="text" placeholder="title" class="form-control shadow-lg">
+    <input type="text" placeholder="discription" class="form-control shadow-lg">
+    <button class="btn btn-danger product-remove-specification" type="button"></button>
+    </div>`);
+  }
+
+  removeSpecification = (event) => {
+    if (event.target.classList.contains("product-remove-specification")) {
+      event.target.parentElement.remove();
+    }
+  }
+
+  photoLink = (event) => {
+    if (event.target.classList.contains("bi-wrench-adjustable-circle")) {
+      event.target.addEventListener("shown.bs.popover", (el) => {
+        let button = document.querySelector(`#${el.target.getAttribute("aria-describedby")} button`);
+        button.addEventListener("click", (event2) => {
+          this.productPhotoContentType(event2.target.previousElementSibling.value).then(result => {
+            if (result) {
+              el.path[1].style.background = `url(${event2.target.previousElementSibling.value})`;
+              el.path[1].style.backgroundColor = `#fff`;
+            }
+          });
+        })
+      })
+    }
+  }
+
+  productPhotoContentType = async (get_url) => {
+    const response = await fetch(get_url);
+    return response.headers.get('content-type').includes("image") ? true : false;
+  }
+
+  addPhotoDiv = () => {
+    this.loadPopOver();
+    if (this.subPhotos.childElementCount < 6) {
+      this.subPhotos.insertAdjacentHTML("beforeend", `<div class="glass second-glass w-100 fs-4 rounded shadow-lg align-items-center justify-content-center"><i class="bi bi-dash-circle remove-photo-div"></i><a href="#" class="bi bi-wrench-adjustable-circle" data-bs-toggle="popover"></a></div>`);
+      this.loadPopOver();
+    }
+  }
+
+  loadPopOver = () => {
+    const popoverTriggerList = this.shadowRoot.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => {
+      if (!popoverTriggerEl.hasAttribute("aria-describedby")) {
+        new bootstrap.Popover(popoverTriggerEl, {
+          container: 'body',
+          title: 'Add Photo Link',
+          html: true,
+          placement: 'bottom',
+          sanitize: false,
+          content: () => {
+            return this.shadowRoot.querySelector('#PopoverContent').innerHTML;
+          }
+        });
+      }
+    });
+  }
+
+  removePhotoDiv = (event) => {
+    if (event.target.classList.contains("remove-photo-div")) {
+      event.target.parentElement.remove();
+    }
+  }
+
+  submitForm = (event) => {
+    event.preventDefault();
+    let productCategory = this.shadowRoot.querySelector("#product-category-add");
+    let product = {
+      timeAddProduct: new Date(),
+      userAddProduct: [crmUser.userName, crmUser.profileID],
+      productID: this.uniqeID(),
+      comments: "",
+      productName: this.shadowRoot.querySelector("#product-name-add").value,
+      productModel: this.shadowRoot.querySelector("#product-model-add").value,
+      productBrand: this.shadowRoot.querySelector("#product-brand-add").value,
+      productPrice: this.shadowRoot.querySelector("#product-price-add").value,
+      productDescription: this.shadowRoot.querySelector("#product-description-add").value,
+      productTags: (this.shadowRoot.querySelector("#product-add-tag").value).split(",").map(el => el.trim()).filter((a) => a),
+      productFeature: [],
+      productSpecification: [],
+      productPhoto: [],
+      show: true,
+    }
+    let productAddLog = {
+      time: new Date(),
+      user: [crmUser.userName, crmUser.profileID],
+      productID: product.productID,
+      logID: this.uniqeID(),
+      logType: "add product",
+      category: productCategory.value,
+      subCategory: productCategory.options[productCategory.selectedIndex].text,
+    }
+    product["productCategory"] = productCategory.value;
+    product["productSubCategory"] = productCategory.options[productCategory.selectedIndex].text;
+    this.shadowRoot.querySelectorAll("#product-feature li").forEach(element => {
+      if (element.children[0].value !== "" && element.children[1].value !== "") {
+        product["productFeature"].push({
+          title: element.children[0].value,
+          discription: element.children[1].value
+        })
+      }
+    });
+    this.shadowRoot.querySelectorAll(".product-specification").forEach(element => {
+      if (element.children[0].value !== "" && element.children[1].value !== "") {
+        product["productSpecification"].push({
+          title: element.children[0].value,
+          discription: element.children[1].value
+        })
+      }
+    });
+    this.shadowRoot.querySelectorAll(".glass").forEach(element => {
+      if (this.detectURLs(element.style.background)) {
+        product["productPhoto"].push(this.detectURLs(element.style.background))
+      }
+    });
+    let apiProduct = `https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/product/${product.productID}.json`
+    let apiLog = `https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/log/${productAddLog.logID}.json`
+    this.postProduct(apiProduct, product);
+    this.postProduct(apiLog, productAddLog);
+  }
+
+  uniqeID = () => {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < 16; i++) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+    return result;
+  };
+
+  detectURLs = (message) => {
+    let urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+    return String(message.match(urlRegex));
+  }
+
+  postProduct = async (apiLink, body) => {
+    let response = await fetch(apiLink, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+    let result = await response.json();
+    return await result;
+  }
+
+  productPhotoContentType = async (get_url) => {
+    const response = await fetch(get_url);
+    return response.headers.get('content-type').includes("image") ? true : false;
+  }
+
+  connectedCallback() {
+    this.loadPopOver();
+    this.productAddSpecification.addEventListener("click", this.addSpecification);
+    this.productSpecification.addEventListener("click", (e) => this.removeSpecification(e));
+    this.mainPhoto.addEventListener("click", (e) => this.photoLink(e));
+    this.subPhotos.addEventListener("click", (e) => this.photoLink(e));
+    this.addPhotoDivBtn.addEventListener("click", this.addPhotoDiv);
+    this.subPhotos.addEventListener("click", (e) => this.removePhotoDiv(e));
+    this.productAddForm.addEventListener("submit", (e) => this.submitForm(e));
+  }
+}
+
+export { addProduct }
