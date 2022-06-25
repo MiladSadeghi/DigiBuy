8cd2e31be443a790660500998918d6ba7088c8e9
Milad Sadeghi <miladsadeghi2323@gmail.com>
2022-06-26 00:32:56 +0430

    CRM - change search function AGAIN! & product registrant work

--- crm/src/components/Product/Search/searchProduct.js
+++ crm/src/components/Product/Search/searchProduct.js
@@ -4,7 +4,7 @@ template.innerHTML = `
 <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="/node_modules/bootstrap-icons/font/bootstrap-icons.css">
 ${window.location.hostname === "miladsadeghi.github.io" ? `<link rel="stylesheet" href="src/components/Product/Search/style.css">` : `<link rel="stylesheet" href="/crm/src/components/Product/Search/style.css">`}
-<div class="main px-3 py-2">
+<div class="main px-3 py-2 position-relative">
   <div class="row m-0 p-0">
     <div class="col p-0">
       <div class="input-group mb-3">
@@ -20,6 +20,7 @@ ${window.location.hostname === "miladsadeghi.github.io" ? `<link rel="stylesheet
     </div>
   </div>
   <div class="content"></div>
+  <div class="product-wait"><i class="bi bi-gear-wide-connected"></i></div>
 </div>
 <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 `;
@@ -30,45 +31,28 @@ class searchProduct extends HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.searchFilter = this.shadowRoot.querySelector("#search-filter");
     this.searchFilterValue = this.searchFilter.value;
-    this.crmProduct().then(result => { this.productObject = result });
+    this.crmProduct().then(result => {(result.ok?this.productWait.classList.add("d-none"):false); return result.json()}).then(result => {this.productObject = result});
     this.filterInput = this.shadowRoot.querySelector("#filter-input");
     this.showContent = this.shadowRoot.querySelector(".content");
+    this.productWait = this.shadowRoot.querySelector(".product-wait");
   }
 
   async crmProduct() {
     const response = await fetch(`https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/product.json`);
-    return await response.json();
+    return response;
   }
 
   searchFilterChange = (e) => {
     this.searchFilterValue = e.target.value;
-    switch (this.searchFilterValue) {
-      case "productID":
-        this.showByIN()
-        break;
-      case "productName":
-        this.showByIN()
-        break;
-    }
-  }
-
-  searchProduct = (e) => {
-    switch (this.searchFilterValue) {
-      case "productID":
-        this.showByIN() // IN = ID and Number
-        break;
-      case "productName":
-        this.showByIN() // IN = ID and Number
-        break;
-    }
+    this.showByIN();
   }
 
   showByIN = () => {
     this.showContent.innerHTML = "";
     for (let [key, value] of Object.entries(this.productObject)) {
-      if (value[this.searchFilterValue].toLowerCase().includes(this.filterInput.value.toLowerCase())) {
+      if ((Array.isArray(value[this.searchFilterValue]) ? value[this.searchFilterValue][0] : value[this.searchFilterValue]).toLowerCase().includes(this.filterInput.value.toLowerCase())) {
         this.showContent.innerHTML += `
-          <div class="card h-100">
+          <div class="card h-100 product-animation">
             <div class="h-100 d-flex align-items-center justify-content-center">
               <img src="${value.productPhoto[0]}" class="card-img-top p-4">
             </div>
@@ -81,14 +65,14 @@ class searchProduct extends HTMLElement {
             </div>
           </div>`;
       }
-    } if(this.filterInput.value === "") {
+    } if (this.filterInput.value === "") {
       this.showContent.innerHTML = "";
     }
   }
 
   connectedCallback() {
     this.searchFilter.addEventListener("change", this.searchFilterChange);
-    this.filterInput.addEventListener("keyup", this.searchProduct);
+    this.filterInput.addEventListener("keyup", this.showByIN);
   }
 }
 
