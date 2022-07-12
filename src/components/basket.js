2638d105da5677123eb550b929d8ab6aec5b9d72
Milad Sadeghi <miladsadeghi2323@gmail.com>
2022-07-12 18:08:04 +0430

    add product to users on db

--- src/components/basket.js
+++ src/components/basket.js
@@ -1,8 +1,9 @@
 class basket{
-  constructor(userID, addToCardBtn, basketElementID) {
+  constructor(userID, addToCardBtn, basketElementID, db) {
     this.userID = userID;
     this.addToCardBtn = addToCardBtn;
     this.basketElementID = basketElementID;
+    this.db = db;
     this.date = new Date();
     this.date.setYear(2080);
   }
@@ -10,36 +11,37 @@ class basket{
   createGuestCookie() {
     let userCookie = Object.fromEntries(document.cookie.split('; ').map(v => v.split(/=(.*)/s).map(decodeURIComponent)));
     let id = this.uniqueID();
-    if(!userCookie.guestID) {
+    if(!userCookie.guestID && !userCookie.userTable) {
       document.cookie = `guestID=${id};path=/;expires=${this.date}`;
       this.userID = id;
-    } else {
+    } else if(userCookie.guestID && !userCookie.userTable) {
       this.userID = userCookie.guestID;
+      this.db = "guest"
+    } else if(userCookie.userTable) {
+      this.userID = userCookie.userTable;
+      this.db = "users";
     }
   }
 
   async getBasket() {
-    let response = await fetch(`https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/guest/${this.userID}/userBasket.json`);
+    let response = await fetch(`https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/${this.db}/${this.userID}/basket.json`);
     let data = await response.json();
-    if(data.length > 0) {
+    if((data !== null? data.length:0) > 0) {
       this.basketElementID.innerHTML += `<span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
       <span class="visually-hidden">New alerts</span>
     </span>`
     }
-    console.log(data);
     return data;
   }
 
   async addToBasket(productID) {
-    console.log(this.addToCardBtn);
     this.addToCardBtn.classList.add("disabled");
     this.addToCardBtn.innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
-    console.log(this.addToCardBtn);
     let basket = await this.getBasket() || [];
     if(!basket.includes(productID)) {
       basket.push(productID);
     }
-    let response = await fetch(`https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/guest/${this.userID}/userBasket.json`, {
+    let response = await fetch(`https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/${this.db}/${this.userID}/basket.json`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json"
@@ -50,7 +52,6 @@ class basket{
     this.getBasket();
     this.addToCardBtn.classList.remove("disabled");
     this.addToCardBtn.innerHTML = `Add To Card`;
-    console.log(data);
   }
 
   uniqueID() {
