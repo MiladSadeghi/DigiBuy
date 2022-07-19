a0e729e36fa057c21ab87136ac424d7b3b55feba
Miald Sadeghi <Miladsadeghi2323@gmail.com>
2022-07-19 22:58:20 +0430

    CRM - Manage user - user review

--- crm/src/components/User/Manage/userManage.js
+++ crm/src/components/User/Manage/userManage.js
@@ -9,23 +9,15 @@ template.innerHTML = `<link rel="stylesheet" href="/node_modules/bootstrap/dist/
       <div show="#userOrder" class="col text-center product-stuff-tab productBodyTabs py-2 fs-5 fw-normal user-tab">
         Orders
       </div>
-      <div show="#wishList" class="col text-center product-stuff-tab productBodyTabs py-2 fs-5 fw-normal user-tab">
-        Wish List
-      </div>
       <div show="#reviews" class="col text-center product-stuff-tab productBodyTabs py-2 fs-5 fw-normal user-tab">
         Reviews
-      </div>
-      <div show="#lastSeen" class="col text-center product-stuff-tab productBodyTabs py-2 fs-5 fw-normal user-tab">
-        Last Seen
       </div>
     </div>
     <div>
       <div class="product-stuff-body bg-white d-flex w-100" style="height: calc(100vh - 111px)">
         <div id="userSetting" class="m-1 p-2 mt-0 w-100"></div>
         <div id="userOrder" class="d-none m-1 p-2 mt-0 w-100"></div>
-        <div id="wishList" class="d-none m-1 p-2 mt-0 w-100"></div>
         <div id="reviews" class="d-none m-1 p-2 mt-0 w-100"></div>
-        <div id="lastSeen" class="d-none m-1 p-2 mt-0 w-100"></div>
       </div>
     </div>
     <div class="wait"><i class="bi bi-gear-wide-connected"></i></div>
@@ -41,11 +33,10 @@ class userManage extends HTMLElement {
     this.tabElements = this.shadowRoot.querySelectorAll(".user-tab");
     this.userSettingDiv = this.shadowRoot.querySelector("#userSetting");
     this.userOrderDiv = this.shadowRoot.querySelector("#userOrder");
-    this.wishListDiv = this.shadowRoot.querySelector("#wishList");
     this.reviewsDiv = this.shadowRoot.querySelector("#reviews");
-    this.lastSeenDiv = this.shadowRoot.querySelector("#lastSeen");
     this.user;
     this.products;
+    this.comments;
   }
 
   get userIDAttribute() {
@@ -57,6 +48,9 @@ class userManage extends HTMLElement {
   }
   getProducts = async () => {
     this.products = await (await fetch(`https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/product.json`)).json();
+  }
+  getComments = async () => {
+    this.comments = await (await fetch(`https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/comments.json`)).json();
   }
 
   tabs = () => {
@@ -156,35 +150,36 @@ class userManage extends HTMLElement {
   }
 
   userOrders = (user, products) => {
-    console.log(user.dashboard["orders"]);
-    for (let [key, value] of Object.entries(user.dashboard["orders"])) {
-      this.userOrderDiv.innerHTML += `
-        <div class="order-item d-flex flex-column border-1 border rounded-3 p-2 mb-2">
-          <div class="order-item-header d-flex justify-content-between">
-            <div class="d-flex align-items-center">
-              <p class="mb-0 text-muted">${value.orderDate}</p>
-              <i class="bi bi-dot mx-2"></i>
-              <p class="mb-0"><span class="text-muted">Product ID</span> ${value.orderID}</p>
-              <i class="bi bi-dot mx-2"></i>
-              <p class="mb-0"><span class="text-muted">Price</span> $${this.moneyFormat(value.orderTotal)}</p>
-              <i class="bi bi-dot mx-2"></i>
-              <p class="mb-0"><span class="text-muted text-capitalize">status</span> ${value.orderStatus}</p>
+    if(user.dashboard["orders"] !== undefined) {
+      for (let [key, value] of Object.entries(user.dashboard["orders"])) {
+        this.userOrderDiv.innerHTML += `
+          <div class="order-item d-flex flex-column border-1 border rounded-3 p-2 mb-2">
+            <div class="order-item-header d-flex justify-content-between">
+              <div class="d-flex align-items-center">
+                <p class="mb-0 text-muted">${value.orderDate}</p>
+                <i class="bi bi-dot mx-2"></i>
+                <p class="mb-0"><span class="text-muted">Order ID</span> ${value.orderID}</p>
+                <i class="bi bi-dot mx-2"></i>
+                <p class="mb-0"><span class="text-muted">Price</span> $${this.moneyFormat(value.orderTotal)}</p>
+                <i class="bi bi-dot mx-2"></i>
+                <p class="mb-0"><span class="text-muted text-capitalize">status</span> ${value.orderStatus}</p>
+              </div>
+            </div>
+            <div class="d-flex gx-2 mt-3">
+              ${(() => {
+                let string = "";
+                value.orderProduct.forEach((item, index) => {
+                  console.log(this.products[item.product].productPhoto[0].replace(`")`, ""));
+                  string += `<div class="position-relative me-3"><img width="80px" src="${this.products[item.product].productPhoto[0].replace(`")`, "")}" alt="">
+                  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">${item.quantity}<span class="visually-hidden">unread messages</span></span>
+                  </div>`;
+                });
+                return string;
+              })()}
             </div>
           </div>
-          <div class="d-flex gx-2 mt-3">
-            ${(() => {
-              let string = "";
-              value.orderProduct.forEach((item, index) => {
-                console.log(this.products[item.product].productPhoto[0].replace(`")`, ""));
-                string += `<div class="position-relative me-3"><img width="80px" src="${this.products[item.product].productPhoto[0].replace(`")`, "")}" alt="">
-                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">${item.quantity}<span class="visually-hidden">unread messages</span></span>
-                </div>`;
-              });
-              return string;
-            })()}
-          </div>
-        </div>
-      `;
+        `;
+      }
     }
   }
 
@@ -192,14 +187,74 @@ class userManage extends HTMLElement {
     return Number(num).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
   }
 
+  userComments = () => {
+    if(this.comments !== null) {
+      this.reviewsDiv.innerHTML = "";
+      for (let [key, value] of Object.entries(this.comments)) {
+        this.reviewsDiv.innerHTML += `<div class="mb-2 d-flex justify-content-between comment-box p-3 align-items-center">
+        <div class="d-flex flex-column w-50">
+          <div class="lh-1 mb-2 d-flex align-items-center">
+            <img width="60px" class="me-2" src="/product/src/IMG/user-avatar.png" />
+            <div>
+              <h5 class="ff-osw mb-1 fs-6">${this.user.userName}</h5>
+              <div class="d-flex">
+                ${(()=> { let string = ""; for (let index = 1; index <= 5; index++) {
+                if(index <=value.rate) { string +=`
+                <div class="fill-star me-1"></div>
+                `; } else { string +=`
+                <div class="empty-star me-1"></div>
+                `; }} return string; })()}
+              </div>
+            </div>
+          </div>
+          <p class="mb-0">${value.comment}</p>
+        </div>
+        <div class="d-flex-flex-column">
+          <a target="_blank" href="/product/?product=${value.productID}">Product</a>
+          <p class="mb-0">${value.date} - ${value.time}</p>
+          <p class="mb-0">${value.commentID}</p>
+        </div>
+        <div class="manage" comment-id="${value.commentID}">
+          <div class="form-check form-switch">
+            <input comment-id="${value.commentID}" class="form-check-input show-comment" type="checkbox" role="switch" ${(value.show === true? "checked": "")}>
+            <label class="form-check-label"">Show</label>
+        </div>
+        </div>
+      </div>`;
+      }
+      let showCheckboxes = this.shadowRoot.querySelectorAll(".show-comment");
+      showCheckboxes.forEach((item, index) => {
+        item.addEventListener("change", async (e)=> {
+          let send = await fetch(`https://digibuy-da839-default-rtdb.europe-west1.firebasedatabase.app/comments/${e.target.getAttribute("comment-id")}.json`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({show: e.target.checked})
+          });
+          if(send.ok) {
+            alert("Comment Updated");
+            await this.getComments();
+            this.userComments();
+          } else {
+            alert(`Try Again Later!. ${send.status}`);
+            e.target.checked = !e.target.checked;
+          };
+        });
+      });
+    }
+  }
+
   async connectedCallback() {
     await this.getUser();
     await this.getProducts();
+    await this.getComments();
     this.loading.classList.add("d-none");
     this.tabs();
     this.tabElements[0].innerHTML = this.user.userName;
     this.userSetting(this.user);
     this.userOrders(this.user, this.products);
+    this.userComments(this.user, this.products);
     console.log(this.user, this.products);
   }
 }
